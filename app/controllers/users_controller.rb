021a656b704a91e0504766f0e8f6c9e68fb592be
chris-jan-dudley <chris.jan.dudley@gmail.com>
2023-05-22 08:05:08 +0100

    Added routes and partial header view  containing login, logout, and register links, with boilerplate session support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.balance = 100
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_path, notice: 'Successfully created account!'
+        else
+            render :new
+        end
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'bank/index'
+  root to: 'bank#index'
   resources :transactions
   resources :users
+
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
+  get 'register', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
