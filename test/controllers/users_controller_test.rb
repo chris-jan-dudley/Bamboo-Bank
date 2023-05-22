e3740209e0137571d26df8976483af2a5f5a9bea
chris-jan-dudley <chris.jan.dudley@gmail.com>
2023-05-22 08:24:44 +0100

    Added test for user creation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "registering a new user creates new user and sets it as current user in session" do
+    post '/users', 
+      params: {user: {name: 'bob', email: 'bob@bamboo.com', password: 'secret', password_confirmation: 'secret'} }
+    assert_response :redirect
+    assert_equal User.last.name, 'bob'
+    assert_equal session[:user_id], User.last.id
+  end
 end
